217e092989164ef1d4f481dac726d60ded3c00cb
Franciele França <92552849+francielefranca@users.noreply.github.com>
2023-12-01 21:46:39 -0300

    Delete projredes directory

--- projredes/projredesapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'projredesapp'
-
-urlpatterns = [
-    path('home', views.home, name='home'),
-    path('traceroute/', views.traceroute, name='traceroute'),
-]
